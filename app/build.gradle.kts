f347f0d01a3c2bfd4f2c5bc5cd882c7702c44c3a
muthuraj20122003 <muthuraj20122003@gmail.com>
2025-12-10 16:25:29 +0000

    Fix build failure: Update NewPipeExtractor dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,7 @@ android {
 
 dependencies {
     coreLibraryDesugaring(libs.desugar.jdk.libs)
-    implementation("com.github.TeamNewPipe:NewPipeExtractor:v0.24.3")
-
+   implementation 'com.github.teamnewpipe:NewPipeExtractor:v0.24.4'
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
